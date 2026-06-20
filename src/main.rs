ebfb6f9451f2ba874ad8802000d506d99e6640f4
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-06-20 18:08:33 -0400

    Se crea gitignore y se juega un poco con la librería Image para ver como funciona

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use image::{open, imageops::FilterType};
+fn open_resize_img(img_path:String, x:u32, y:u32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let img = open(img_path).unwrap();
+    let resimg = img.resize(x, y, FilterType::Lanczos3);
+    let resimg: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = resimg.into_rgb8();
+    resimg
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let img_path: String = String::from("img.jpg");
+    let images: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = open_resize_img(img_path, 100, 100);
+    for i in images.pixels() {
+        print!("({}, {}, {})", i[0], i[1], i[2]);
+    }
 }
